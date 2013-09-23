9a2f438fc0fc6153619834e332f18458b22b328f
Rcalaf <rcalaf.cot@gmail.com>
2013-09-23 23:13:30 +0200

    schedule config

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,8 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-job_type :custom_rake,    "cd :path && :environment_variable=:environment bundle exec rake :task --silent"
+set :job_template, "bash -l -c 'cd :path && :job'"
+
 # Example:
 #
 set :output, "#{Rails.root}/log/cron_log.log"
@@ -14,8 +15,11 @@ set :output, "#{Rails.root}/log/cron_log.log"
 #   rake "some:great:rake:task"
 # end
 #
+
+ job_type :custom,    "cd :path && :environment_variable=:environment bundle exec rake :task --silent"
+ 
  every 5.minutes do
-   custom_rake "apn:notifications:deliver"
+   custom "apn:notifications:deliver"
  end
 
 # Learn more: http://github.com/javan/whenever
