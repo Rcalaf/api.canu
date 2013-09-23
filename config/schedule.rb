a0cacab7d96462551d2871821550016ba7ee9af1
Rcalaf <rcalaf.cot@gmail.com>
2013-09-24 00:08:39 +0200

    schedule config

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,8 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-set :job_template, "bash -l -c ':job' >/dev/null 2>&1"
+env :PATH, '/bin/bash'
+#set :job_template, "bash -l -c ':job'"
 
 # Example:
 #
@@ -16,7 +17,7 @@ set :job_template, "bash -l -c ':job' >/dev/null 2>&1"
 # end
 #
 
- job_type :custom_rake, "cd :path && RAILS_ENV=:environment bundle exec rake :task --silent "
+ job_type :custom_rake, "cd :path && RAILS_ENV=:environment bundle exec rake :task --silent"
  
  every 5.minutes do
    custom_rake "apn:notifications:deliver"
