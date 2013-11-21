7898524ca8398df5a2e58af33ce4f6a7956d0ba1
Rcalaf <rcalaf.cot@gmail.com>
2013-11-21 20:09:53 +0100

    added extra data APN

--- app/models/message.rb
+++ app/models/message.rb
@@ -15,7 +15,7 @@ class Message < ActiveRecord::Base
      receivers.each do |user|
        user.devices.each do |device| 
           device.update_attribute(:badge,device.badge.to_i + 1)
-          notifications << APNS::Notification.new(device.token,{:alert => "#{self.user.user_name}@#{self.activity.title} Chat:\n#{self.text[0..(101-self.activity.title.size-self.user.user_name.size)]}...",:badge => device.badge,:sound => 'default'})
+          notifications << APNS::Notification.new(device.token,{:alert => "#{self.user.user_name}@#{self.activity.title} Chat:\n#{self.text[0..(101-self.activity.title.size-self.user.user_name.size)]}...",:badge => device.badge,:sound => 'default',:other => {:activity_id => self.activity.id}})
        end
      end
      APNS.send_notifications(notifications) unless notifications.empty?
