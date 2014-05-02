308f5ba372b79a33dd9175572903edaa36334c13
VivienCormier <vivien.cormier@gmail.com>
2014-05-02 16:11:18 +0200

    Add total local / tribe

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,7 +1,7 @@
 class StatisticsController < ApplicationController
   def index
     user = {total: User.all.size, ghost: Ghostuser.where(isLinked: false).size, total_phone_verified: User.where(phone_verified: true).size, total_email_verified: User.where(active: true).size, not_verified: User.where('(active = false || active is null) && (phone_verified = false || phone_verified is null) ').size }
-    activity = {total: Activity.all.size, total_active: Activity.active(Time.zone.now).size, active_activities_locations: Activity.select('latitude,longitude').active(Time.zone.now).privacy_location(true), active_activities_locations_local: Activity.select('latitude,longitude').active(Time.zone.now).privacy_location(false)}
+    activity = {total: Activity.all.size, total_active: Activity.active(Time.zone.now).size,total_active_local: Activity.active(Time.zone.now).privacy_location(false).size,total_active_tribe: Activity.active(Time.zone.now).privacy_location(true).size, active_activities_locations: Activity.select('latitude,longitude').active(Time.zone.now).privacy_location(true), active_activities_locations_local: Activity.select('latitude,longitude').active(Time.zone.now).privacy_location(false)}
     render json: {user: user, activity: activity}, status: 200
   end
 end
