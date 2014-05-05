e9bfb013cb4bbe2a949833ed96efcceb7f5e3a5b
VivienCormier <vivien.cormier@gmail.com>
2014-05-05 15:19:29 +0200

    Disable Activity_notifications

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -34,7 +34,7 @@ class Activity < ActiveRecord::Base
   }
   
   belongs_to :user 
-  has_many :activity_notifications, dependent: :destroy
+  # has_many :activity_notifications, dependent: :destroy
   
   has_and_belongs_to_many :attendees,
                           class_name: "User",
--- app/models/device.rb
+++ app/models/device.rb
@@ -2,7 +2,7 @@ class Device < ActiveRecord::Base
   attr_accessible :token, :user_id, :badge
   
   belongs_to :user
-  has_many :activity_notifications, order: 'created_at', dependent: :destroy
+  # has_many :activity_notifications, order: 'created_at', dependent: :destroy
   
   validates :token, :presence => true
   validates :token, :uniqueness => true
